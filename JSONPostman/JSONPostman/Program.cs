70749a287f4f71b576eba6af5a16099545161061
mburly <mburleson@cfl.rr.com>
2022-07-06 23:21:46 -0400

    System-JSON submission

--- JSONPostman/JSONPostman/Program.cs
+++ JSONPostman/JSONPostman/Program.cs
@@ -8,7 +8,7 @@ namespace JSONPostman
         public static void Main(string[] args)
         {
             string dataPath = $"{FileRoot.Root}{Path.DirectorySeparatorChar}Data{Path.DirectorySeparatorChar}";
-            string[] bookNames = {"Babbitt", "Gameofthrones", "Midnightinchernobyl", "Mynameisasherlev", "Schindlerslist"};
+            string[] bookNames = { "Babbitt", "Gameofthrones", "Midnightinchernobyl", "Mynameisasherlev", "Schindlerslist" };
             foreach (string book in bookNames)
             {
                 string path = $"{dataPath}{book}.json";
@@ -17,7 +17,7 @@ namespace JSONPostman
                 {
                     jsonString = sr.ReadToEnd();
                 }
-                var options = new JsonSerializerOptions{PropertyNameCaseInsensitive = true};
+                var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
                 var b = JsonSerializer.Deserialize<Book>(jsonString, options);
                 Console.WriteLine(b);
                 Console.WriteLine("======================================================================");
@@ -26,7 +26,7 @@ namespace JSONPostman
             string[] authors = { "Wise Brown", "Margaret Wise Brown" };
             VolumeInfo vi = new VolumeInfo("Goodnight Moon", "In a great green room, tucked away in bed, is a little bunny. \"Goodnight room, goodnight moon.\" And to all the familiar things in the softly lit room—to the picture of the three little bears sitting in chairs, to the clocks and his socks, to the mittens and the kittens, to everything one by one—he says goodnight.", authors);
             Item i = new Item("L5bxPI2YhfEC", "https://www.googleapis.com/books/v1/volumes/L5bxPI2YhfEC", vi);
-            
+
             List<Item> details = new List<Item>();
             details.Add(i);
             Book foo = new Book(details);
